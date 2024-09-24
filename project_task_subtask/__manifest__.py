4c90b63593e512813de85bcf0d54eb8975adc54e
Eugene Molotov <molotov@it-projects.info>
2024-09-24 14:03:35 +0500

    [ADD] project_task_subtask: модуль из 14.0
    Co-authored-by: Ivan Yelizariev <yelizariev@users.noreply.github.com>
    Co-authored-by: Rafael Manaev <manawi@users.noreply.github.com>
    Co-authored-by: Ilmir Karamov <ilmir-k@users.noreply.github.com>
    Co-authored-by: Ildar Nasyrov <iledarn@users.noreply.github.com>
    Co-authored-by: Nicolas Jeudy <njeudy@users.noreply.github.com>
    Co-authored-by: Alexandr Kolushov <KolushovAlexandr@users.noreply.github.com>
    Co-authored-by: Almas Giniatullin <almas50@users.noreply.github.com>
    Co-authored-by: Denis Mudarisov <trojikman@users.noreply.github.com>
    Co-authored-by: Vildan Safin <Enigma228322@users.noreply.github.com>
    Co-authored-by: Eugene Molotov <em230418@users.noreply.github.com>
    Co-authored-by: Victor Bykov <BykovVik@users.noreply.github.com>

---
+++ project_task_subtask/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": """Project Task Checklist""",
+    "summary": """Use checklist to be ensure that all your tasks are performed and to make easy control over them""",  # noqa: B950
+    "category": """Project Management""",
+    "images": ["images/checklist_main.png"],
+    "version": "14.0.1.1.2",
+    "author": "IT-Projects LLC",
+    "support": "it@it-projects.info",
+    "website": "https://github.com/it-projects-llc/misc-addons",
+    "license": "LGPL-3",
+    "depends": ["project"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/project_task_subtask.xml",
+        "views/assets.xml",
+        "data/subscription_template.xml",
+    ],
+    "qweb": ["static/src/xml/templates.xml"],
+    "demo": ["demo/project_task_subtask_demo.xml"],
+}
